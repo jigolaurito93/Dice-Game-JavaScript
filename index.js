b7855d9076b52eaa1b7b80df0274bd638dc2b851
Jose Laurito <jigolaurito93@yahoo.com>
2023-08-09 02:05:37 -0500

    added flag emoji

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ document.querySelector(".img1").setAttribute("src", "/images/dice"+randomNumber1
 document.querySelector(".img2").setAttribute("src", "/images/dice"+randomNumber2+ ".png");
 
 if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").innerHTML = "Player 1 Wins!";
+    document.querySelector("h1").innerHTML = "🚩Player 1 Wins!";
 } else if (randomNumber1 < randomNumber2) {
-    document.querySelector("h1").innerHTML = "Player 2 Wins!";
+    document.querySelector("h1").innerHTML = "Player 2 Wins!🚩";
 }  else {
     document.querySelector("h1").innerHTML = "Draw!";
 }
